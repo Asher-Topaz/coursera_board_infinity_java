ba33767b7d4743217b1c770cd74d8de8635959f0
ashertopaz <asheranumah@gmail.com>
2024-07-16 17:22:07 -0400

    tutorials

---
+++ src/Basics/VariablesAndDataTypes.java
@@ -0,0 +1,22 @@
+package Basics;
+
+public class VariablesAndDataTypes {
+    public static void main(String[] args){
+        //primitive data types
+
+        byte age = 23;
+        long viewsCount = 1_112_345_678;
+        float price = 234.56F;
+        char gender = 'M';
+        boolean isValid = true;
+        System.out.println(age);
+        System.out.println(viewsCount);
+        System.out.println(price);
+        System.out.println(gender);
+        System.out.println(isValid);
+
+        
+
+
+    }
+}
